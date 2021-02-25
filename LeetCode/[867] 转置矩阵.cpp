892fe05a251850329e292e6a0169148f81a5ec91
looeyWei <congwei_huang@163.com>
2021-02-25 15:13:01 +0800

    每日一题

---
+++ LeetCode/[867] 转置矩阵.cpp
@@ -0,0 +1,20 @@
+
+#include <vector>
+using namespace std;
+
+////////////// 以下是原答案 ///////////////////
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        vector<vector<int>> res(n, vector<int>(m));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
